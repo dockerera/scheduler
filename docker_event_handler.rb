4dd8a950cc4de3d92e73f921c2f48f1c54f5a83c
Frank Lemanschik <frank@github.dspeed.eu>
2014-09-20 19:50:32 +0200

    Update and rename docker_event_handler to docker_event_handler.rb

--- docker_event_handler.rb
+++ docker_event_handler.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # Docker Event Listener / DDNS
 # Author: Kelly Becker <kbecker@kellybecker.me>
+# She is a nice girl i call her my Cherry Apple because she works at Apple but is more elegant like a Cherry :D
 # Website: http://kellybecker.me
 # Original Code: https://gist.github.com/KellyLSB/4315a0323ed0fe1d79b6
 # License: MIT
@@ -76,48 +77,12 @@ while line = events.gets
 
   case ENV['CONTAINER_EVENT']
   when 'start'
-
-    # Get Container Details for DNS
-    ENV['CONTAINER_IP_ADDR']  = %x{docker inspect $CONTAINER_CID_LONG | grep '"IPAddress"'}.strip.gsub(/[^0-9\.]/i, '')
-    ENV['CONTAINER_HOSTNAME'] = %x{docker inspect $CONTAINER_CID_LONG| grep '"Hostname"' | awk '{print $NF}'}.strip.gsub(/^"(.*)",/i, '\1')
-
-    puts ENV['CONTAINER_HOSTNAME']
-    puts ENV['CONTAINER_IP_ADDR']
-
-    # Get Process ID of the LXC Container
-    ENV['NSPID'] = %x{head -n 1 $(find "$CGROUPMNT" -name $CONTAINER_CID_LONG | head -n 1)/tasks}.strip
-
-    # Ensure we have the PID
-    unless ENV['NSPID']
-      log.error "Could not find a process indentifier for container #{ENV['CONTAINER_CID']}. Cannot update DNS."
-      next
-    end
-
-    # Create the Net Namespaces
-    %x{mkdir -p /var/run/netns}
-    %x{rm -f /var/run/netns/$NSPID}
-    %x{ln -s /proc/$NSPID/ns/net /var/run/netns/$NSPID}
-
-    # Build the command to update the dns server
-    update_command = <<-UPDATE
-    ip netns exec $NSPID nsupdate -k $DDNS_KEY <<-EOF
-    server $NET_NS
-    zone $NET_DOMAIN.
-    update delete $CONTAINER_HOSTNAME.$NET_DOMAIN
-    update add $CONTAINER_HOSTNAME.$NET_DOMAIN 60 A $CONTAINER_IP_ADDR
-    send
-    EOF
-    UPDATE
-
-    # Run the nameserver update in the Net Namespace of the LXC Container
-    system(update_command.gsub(/^[ ]{4}/, ''))
-
-    # Message an success
-    if $?.success?
-      log.info "Updated Docker DNS (#{ENV['CONTAINER_CID']}): #{ENV['CONTAINER_HOSTNAME']}.#{ENV['NET_DOMAIN']} 60 A #{ENV['CONTAINER_IP_ADDR']}."
-    else
-      log.error "We could not update the Docker DNS records for #{ENV['CONTAINER_CID']}. Please check your nsupdate keys."
-    end
+      #Run Add to Bind9
+      # add-zone.sh $CONTAINER_CID_LONG
+      docker restart proxy
+  end
+  when 'stop'
+      docker restart proxy
   end
 end
 
